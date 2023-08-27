b287e33a4833762b6c59321e335c3fbc57aaf3f2
Everton Pinheiro <evertonpinheiroti@gmail.com>
2023-08-27 18:29:10 -0300

    feat(auth): added signin

---
+++ api/src/modules/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SigninDto, SignupDto } from 'src/modules/auth/dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signin')
+  @HttpCode(200)
+  signin(@Body() signinDto: SigninDto) {
+    return this.authService.signin(signinDto);
+  }
+
+  @Post('signup')
+  signup(@Body() signupDto: SignupDto) {
+    return this.authService.signup(signupDto);
+  }
+}
